906a68ba89cc067a9bff506df4bb1a48e984e0ff
putian74j <18915031666@189.cn>
2020-02-16 21:51:11 +0800

    create data2

--- Code/create_date.py
+++ Code/create_date.py
@@ -8,9 +8,21 @@ fake = Faker('Zh-cn')
 
 def create_user():
 	for i in range(10):
-		user = user(name=fake.name(),email=fake.email())
+		user = User(name=fake.name(),email=fake.email())
 
 		session.add(user)
 
 def create_course():
-	for 
+	for user in session.query(User).all():
+		for i in range(2):
+			course = Course(name=''.join(fake.words(4)),user_id = user.id)
+			session.add(course)
+
+def main():
+
+	create_user()
+	create_course()
+	session.commit()
+
+if __name__ == '__main__':
+	main()
